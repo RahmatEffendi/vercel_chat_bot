a8135821f7778ad8ba2d38aed2fe1a06fdf242a7
rahmateffendi <rahmateffendi@jukesolutions.com>
2024-09-25 10:46:22 +0700

    change port

--- api/index.js
+++ api/index.js
@@ -55,6 +55,6 @@ app.post("/sendError", (req, res) => {
     return res.send("second");
 });
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.listen(3001, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
